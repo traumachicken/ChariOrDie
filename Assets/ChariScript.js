2f6c73e5de9d9bd76bde191758bfbde99223ea3f
Traumachicken <traumachicken@gmail.com>
2011-05-13 22:34:05 +0900

    controll chari

--- Assets/ChariScript.js
+++ Assets/ChariScript.js
@@ -1,34 +1,54 @@
 var other : GUIScript;
 var start_position : Vector3 ;
-var friction = 0;
+
+
+public var accel : float = 0.15 ;
+public var friction : float = 0.995 ;
+public var maxSpeed : float = 30 ;
+public var rotationSpeed : float = 8.0;
+public var jumpSpeed : float = 50.0 ;
+public var gravity : float = 1.0 ;
+
+private var vertical : float = 0 ;
+private var horizonal : float = 0;
+private var isControllable : boolean  = true ;
+private var controller : CharacterController ;
+private var moveDirection : Vector3 = Vector3.zero ;
 
 function Start () {
-    // Find the OtherScript which is attached to any game object in the scene.
-    other = FindObjectOfType(GUIScript);
 	start_position = transform.position;
+	controller = GetComponent( CharacterController );
 }
+
 function Update() {
-
-    transform.Rotate(0, other.speed, 0);
-    transform.Translate(other.speed, 0, 0);
 	
-}
-
-function Restart() {
-	transform.position = start_position;
-	transform.rotation = Quaternion(0,0,0,0);
-}
-
-function OnCollisionStay(collisionInfo : Collision) {
-	//~ transform.Rotate(0, other.speed, 0);
-    //~ transform.Translate(other.speed, 0, 0);
-	//~ rigidbody.AddRelativeForce(Vector3.forward * other.speed);
-	//~ rigidbody.rotation = Quaternion(0, other.speed, 0, 0);
-	//~ rigidbody.AddForce(Vector3.right * other.speed * 40);
-	//~ rigidbody.velocity = Vector3( other.speed * 10, 0, 0 );
+    if (controller.isGrounded) {
+		
+		horizonal = 0 ;
+		
+		vertical += Input.GetAxis("Vertical") * accel ;
+		if ( vertical > maxSpeed )	vertical = maxSpeed ;
+        
+        
+        var rot : float = Input.GetAxis("Horizontal") ;
+		if( vertical < 3.0 )	rot *= vertical / 3.0 ;
+        controller.transform.Rotate( 0, rot, 0 );
+        if (Input.GetButton ("Jump")) {
+            horizonal = jumpSpeed;
+        }
+    }
+	
+    // Apply gravity
+    horizonal -= gravity ;
     
-}
-
-function OnCollisionEnter(collisionInfo:Collision){
+	// Don't back
+	if ( vertical < -1.5 )	vertical = -1.5 ;
 	
+    // Move the controller
+    moveDirection = Vector3( 0, horizonal, vertical );
+	moveDirection = transform.TransformDirection(moveDirection);
+	
+	controller.Move(moveDirection * Time.deltaTime);
+	
+	vertical *= friction ;
 }
